8217ecff6f089c57133831cbeba117f83e3b967c
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-15 12:47:51 +0800

    test : done mock test session1

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -3,6 +3,7 @@ package cashregister;
 
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 public class CashRegisterTest {
@@ -11,15 +12,18 @@ public class CashRegisterTest {
     @Test
     public void should_print_the_real_purchase_when_call_process() {
         //given
+        Printer printer = mock(Printer.class);
+        CashRegister cashRegister = new CashRegister(printer);
+        Purchase purchase = new Purchase(new Item[0]);
         //when
+        cashRegister.process(purchase);
         //then
+        verify(printer,times(1)).print("");
     }
 
     @Test
     public void should_print_the_stub_purchase_when_call_process() {
-        //given
-        //when
-        //then
+
     }
 
     @Test
